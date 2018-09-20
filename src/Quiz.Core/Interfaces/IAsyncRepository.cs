d8f0974b270fb8ec92545cc09080eede05c08e5f
Md. Aftab Uddin Kajal <kajal.alpha@gmail.com>
2018-09-20 17:41:01 +0600

    repository added

---
+++ src/Quiz.Core/Interfaces/IAsyncRepository.cs
@@ -0,0 +1,21 @@
+﻿using Quiz.Core.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.Core.Interfaces
+{
+    public interface IAsyncRepository<T> where T : Entity
+    {
+        Task<T> GetByIdAsync(Guid id);
+
+        Task<IEnumerable<T>> ListAllAsync();
+
+        Task<T> AddAsync(T entity);
+
+        Task UpdateAsync(T entity);
+
+        Task DeleteAsync(T entity);
+    }
+}
