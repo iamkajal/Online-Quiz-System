40278017473637286dc99f0abcb8b39da7e3fdaf
Md. Aftab Uddin Kajal <kajal.alpha@gmail.com>
2018-09-20 17:34:31 +0600

    DbConnection Add

---
+++ src/Quiz.Infrastructure/Data/QuizTestContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quiz.Infrastructure.Data
+{
+  public  class QuizTestContext:DbContext
+    {
+        public QuizTestContext(DbContextOptions<DbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
